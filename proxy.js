cc178922c668c834edb78aa79932bb65c9166710
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-12-13 21:46:54 +0700

    a

--- proxy.js
+++ proxy.js
@@ -21,11 +21,12 @@ function start_proxy_server(){
 	var options
 	try {
 		options = get_proxy_table(proxy_table);
-	} finally {
 		proxy_server.close = proxy_server.close || function(){};
 		proxy_server.close();
 		proxy_server = httpProxy.createServer(options);
 		proxy_server.listen(8080);
+	} catch (e){
+
 	}
 }
 
@@ -55,15 +56,19 @@ function start_static_servers(){
 	var table;
 	try {
 		table = get_static_table(static_table);
-	} finally {
-		static_servers = [];
 		for (var i = static_servers.length - 1; i >= 0; i--) {
 			static_servers[i].close = static_servers[i].close || function(){};
 			static_servers[i].close();
 		};
+		static_servers = [];
 		for (port in table) {
-			static_servers.push(start_a_static_server(port,table[port]));
+			try {
+				static_servers.push(start_a_static_server(port,table[port]));
+			} catch (e) {
+			};
 		};
+	} catch (e) {
+		
 	}
 }
 
